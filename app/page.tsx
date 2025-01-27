8743db6749ef08c51de4b594070c6e3667d28629
sujansijakhow <sujansijakhow@gmail.com>
2025-01-27 20:37:34 +0545

    NewFeature added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 
 import Navbar from "./components/Navbar"
 import TopBackground from "./components/Hero/TopBackground";
+import Section from "./components/Section";
 
 export default function Home() {
   return (
     <div className="relative overflow-clip" id="home">
-      <div className="w-screen h-screen"></div>
+      <Section />
       <Navbar />
       <TopBackground />
     </div>
