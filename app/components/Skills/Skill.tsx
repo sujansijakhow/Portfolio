0ddb2500d45f0c956f6033543c013a0df7b13d04
sujansijakhow <sujansijakhow@gmail.com>
2025-01-29 16:16:41 +0545

    Skill feature added

--- app/components/Skills/Skill.tsx
+++ app/components/Skills/Skill.tsx
@@ -1,4 +1,8 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useRef } from 'react'
+import {motion, useMotionTemplate, useMotionValue} from "framer-motion"
+import Reveal from '../Reveal'
 
 type Props = {
     icon: string,
@@ -6,11 +10,46 @@ type Props = {
 }
 
 const Skill = ({icon, name}: Props) => {
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const xDistance = useMotionValue(0);
+  const yDistance = useMotionValue(0);
+
+  const mask = useMotionTemplate `radial-gradient(100px 100px at ${xDistance}px ${yDistance}px, #000, transparent)`
+
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if(!ref.current) return
+
+    const clientRect = ref.current.getBoundingClientRect()
+
+    xDistance.set(e.x -clientRect.x)
+    yDistance.set(e.y -clientRect.y)
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove)
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [])
+
   return (
-    <div className='relative flex gap-2 p-2 border-primary border rounded-lg h-[46px]'>
-        <img src={icon} alt={`${name} icon`} />
-        <p className='text-lg'>{name}</p>
-    </div>
+    <Reveal duration={1.5}>
+      <div className='relative flex gap-2 p-2 border-primary border rounded-lg h-[46px]'>
+          <motion.div
+            ref={ref}
+            className='absolute inset-0 border-2 border-purple-500 rounded-lg'
+            style={{
+              maskImage: mask,
+              WebkitMaskBoxImage: mask,
+            }}></motion.div>      
+          <img src={icon} alt={`${name} icon`} />
+          <p className='text-lg'>{name}</p>
+      </div>
+    </Reveal>
   )
 }
 
