b1fcf697b9296087c3ce9c75ee4c18e05c3f8eaa
sujansijakhow <sujansijakhow@gmail.com>
2025-02-05 20:34:07 +0545

    Link Url updated

--- app/components/Projects/Project.tsx
+++ app/components/Projects/Project.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {motion, useMotionValue, useSpring, useTransform} from "framer-motion";
+import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
 
 import React, { MouseEventHandler } from 'react'
 import Image from 'next/image';
@@ -28,20 +28,20 @@ const Project = ({
   const x = useMotionValue(0)
   const y = useMotionValue(0)
 
-  const xSpring =useSpring(x)
-  const ySpring =useSpring(y)
+  const xSpring = useSpring(x)
+  const ySpring = useSpring(y)
 
   const xRotation = useTransform(ySpring, [-0.5, 0.5], ["15deg", "-15deg"])
   const yRotation = useTransform(xSpring, [-0.5, 0.5], ["-15deg", "15deg"])
 
   const handleMouseMove: MouseEventHandler<HTMLDivElement> = (e) => {
-    if(!e.currentTarget) return
+    if (!e.currentTarget) return
 
     const target = e.currentTarget as HTMLElement;
     const clientRect = target.getBoundingClientRect()
 
-    const xPos = (e.clientX -clientRect.left) / clientRect.width -0.5
-    const yPos = (e.clientY -clientRect.top) / clientRect.height -0.5
+    const xPos = (e.clientX - clientRect.left) / clientRect.width - 0.5
+    const yPos = (e.clientY - clientRect.top) / clientRect.height - 0.5
 
     x.set(xPos)
     y.set(yPos)
@@ -60,30 +60,36 @@ const Project = ({
           rotateX: xRotation,
           rotateY: yRotation,
         }}>
-          <Image src={thumbnail} alt={`Thumbnail for ${title}`} width={392} height={230}
-            className='w-full h-[230px] object-cover' style={{
-              transform: "translateZ(100px)",
-            }} />
-            <div className='flex flex-col gap-[11px]'>
-              <div className='flex justify-between items-center'>
-                <h3 className='font-semibold text-xl md:text-[22px]'>{title}</h3>
-                <a href={link.url} className='flex px-[5px] py-[3px] gap-1 bg-[#b9b9b9]
-                  bg-opacity-[24%] rounded text-[14px]'>
-                    <span className='hidden md:block'>{link.label}</span>
-                    <img src="/link_arrow.svg" alt="Link arrow" className='block dark:hidden' />
-                    <img src="/link_arrow_dark.svg" alt="Link Dark arrow" className='hidden dark:block' />
-                  </a>
-              </div>
-              <p className='line-clamp-2 text-sm md:text-base'>{description}</p>
-              <div className='flex flex-row gap-[11px]'>
-                {languageIcons.map((icon, iconId) => (
-                    <img src={icon} alt='Language Icons' key={iconId} />
-                  ))
-                }
-              </div>
-            </div>
-        </motion.div>
-      </Reveal>
+        <Image src={thumbnail} alt={`Thumbnail for ${title}`} width={392} height={230}
+          className='w-full h-[230px] object-cover' style={{
+            transform: "translateZ(100px)",
+          }} />
+        <div className='flex flex-col gap-[11px]'>
+          <div className='flex justify-between items-center'>
+            <h3 className='font-semibold text-xl md:text-[22px]'>{title}</h3>
+            <a
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className='flex px-[5px] py-[3px] gap-1 bg-[#b9b9b9]
+              bg-opacity-[24%] rounded text-[14px]'
+            >
+              <span className='hidden md:block'>{link.label}</span>
+              <img src="/link_arrow.svg" alt="Link arrow" className='block dark:hidden' />
+              <img src="/link_arrow_dark.svg" alt="Link Dark arrow" className='hidden dark:block' />
+            </a>
+
+          </div>
+          <p className='line-clamp-2 text-sm md:text-base'>{description}</p>
+          <div className='flex flex-row gap-[11px]'>
+            {languageIcons.map((icon, iconId) => (
+              <img src={icon} alt='Language Icons' key={iconId} />
+            ))
+            }
+          </div>
+        </div>
+      </motion.div>
+    </Reveal>
   )
 }
 
